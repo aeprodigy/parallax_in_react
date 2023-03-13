040bf77118fa952f18c424d064a2530884e83a20
mike mambwe <mikeapple056@gmail.com>
2023-03-13 09:00:35 +0200

    js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import './App.css';
+
+function App() {
+  const bg = "https://pixabay.com/images/id-3608029/"
+  return (
+    <div className="App">
+      <h1> Error 404 Page coming soon </h1>
+	  <p>Mambwe Mike</p>
+    </div>
+  );
+}
+
+export default App;
