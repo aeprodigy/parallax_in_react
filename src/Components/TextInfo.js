db1c361d57631b355bf6ec460a2832dc7b191574
mike mambwe <mikeapple056@gmail.com>
2023-03-14 14:34:01 +0200

    textInfo

---
+++ src/Components/TextInfo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TextInfo = () => {
+  return (
+    <div className='text-box'>
+        <h3>Buy and Drive</h3>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum rerum fuga fugit dolor sapiente animi, harum quaerat maxime non, officia aut, quas reiciendis. Explicabo, rerum exercitationem reprehenderit similique quibusdam accusamus?
+        </p>
+    </div>
+  )
+}
+
+export default TextInfo
